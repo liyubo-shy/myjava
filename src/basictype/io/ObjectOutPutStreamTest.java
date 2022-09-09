8aaba67ca88359fb0342ccf3b69aef23bd53384b
liyubo-shy <859591603@qq.com>
2022-09-10 05:30:57 +0800

    2022/09/10 IO_序列化

--- src/basictype/io/ObjectOutPutStreamTest.java
+++ src/basictype/io/ObjectOutPutStreamTest.java
@@ -45,4 +45,12 @@ class Dog implements Serializable{
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
