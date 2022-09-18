36bd672af824cceb80bd13087ccbc12d8f3a540f
liyubo-shy <859591603@qq.com>
2022-09-18 09:15:18 +0800

    2022/09/18 Net_Socket

--- src/Inet/b2/SocketTestClient.java
+++ src/Inet/b2/SocketTestClient.java
@@ -7,10 +7,14 @@ import java.net.Socket;
 
 public class SocketTestClient {
     public static void main(String[] args) throws IOException {
+        //连接本机的8848端口服务，连接成功创建socket
         Socket socket = new Socket(InetAddress.getLocalHost(), 8848);
         System.out.println("连接服务端成功！");
+        //通过socket获取跟该连接关联的流对象
         OutputStream outputStream = socket.getOutputStream();
+        //输出数据
         outputStream.write("hello excel".getBytes());
+        //关闭资源
         outputStream.close();
         socket.close();
         System.out.println("退出连接");
--- src/Inet/b2/SocketTestServer.java
+++ src/Inet/b2/SocketTestServer.java
@@ -7,16 +7,21 @@ import java.net.Socket;
 
 public class SocketTestServer {
     public static void main(String[] args) throws IOException {
+        //创建ServerSocket，监听8848端口
         ServerSocket serverSocket = new ServerSocket(8848);
         System.out.println("服务端正在等待连接...");
+        //连接成功创建socket对象
         Socket socket = serverSocket.accept();
         System.out.println("连接成功！");
+        //通过socket获取跟该连接关联的流对象
         InputStream inputStream = socket.getInputStream();
+        //开始读取客户端数据
         byte[] buf = new  byte[1024];
         int len = 0;
         while ((len = inputStream.read(buf)) != -1){
             System.out.println(new String(buf,0,len));
         }
+        //关闭资源
         serverSocket.close();
         inputStream.close();
         socket.close();
